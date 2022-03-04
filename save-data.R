eac1a5bfa271d9d124cae1fcc15cab9e92e004fc
Bjorn Kallerud <bjornkallerud@gmail.com>
2022-03-04 10:08:12 -0800

    Updated LTC data

--- save-data.R
+++ save-data.R
@@ -2,4 +2,4 @@ setwd("K:/IR&D/IR21-03 HMA Improvements/R/dataOPS")
 
 df_rank_pg <- readxl::read_xlsx("data-raw/paygrades.xlsx")
 
-usethis::use_data(df_rank_pg)
+usethis::use_data(df_rank_pg, overwrite = T)
